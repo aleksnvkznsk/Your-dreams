7bdb7592a901d3cb961a6446335fd8acee3b1bfe
aleksnvkznsk <aleksandrov.developer@gmail.com>
2024-08-18 22:48:56 +0700

    scss connection

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,19 @@
-import "./Navbar.css";
+import "./Navbar.scss";
 import logo from "./../../assets/logo.png"
 
 function Navbar() {
   return (
     <>
       <div className="navbar">
-        <div className="logo">
-            <img src={logo} alt=""/>
-          <p className="logo__text">Hounter</p>
+        <div className="navbar__logo">
+            <img className="navbar__logo__img" src={logo} alt=""/>
+          <p className="navbar__logo__text">Hounter</p>
         </div>
-        <div className="buttons">
-            <button className="button">About Us</button>
-            <button className="button">Article</button>
-            <button className="button">Property</button>
-            <button className="button__green">Sing Up!</button>
+        <div className="navbar__buttons">
+            <button className="navbar__buttons__button">Article</button>
+            <button className="navbar__buttons__button">About Us</button>
+            <button className="navbar__buttons__button">Property</button>
+            <button className="navbar__buttons__button__green">Sing Up!</button>
         </div>
       </div>
     </>
